6c73046739eba4fb9a47bea35f0de5ec189afb7a
03kkim <theq614@gmail.com>
2023-01-10 15:12:32 -0500

    add change text color on hover and also cursor pointer

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,20 @@ export default function Home() {
   return (
     <div className="font-open-sans font-light bg-gradient-to-b from-background to-white h-screen">
       <div className="flow-root">
-        <div className="py-10 pl-10 float-left">
+        <div className="p-10 float-left">
           <h1 className="text-6xl">Kyu Hong Kim</h1>
         </div>
-        <div className="py-10 pr-10 space-x-10 text-4xl float-right">
-          <h2 className="inline-block">Bio</h2>
-          <h2 className="inline-block">Dev</h2>
-          <h2 className="inline-block">Music</h2>
-          <h2 className="inline-block">Contact</h2>
+        <div className="p-10 space-x-10 text-4xl float-right">
+          <h2 className="inline-block hover:text-white cursor-pointer">
+            About
+          </h2>
+          <h2 className="inline-block hover:text-white cursor-pointer">Dev</h2>
+          <h2 className="inline-block hover:text-white cursor-pointer">
+            Music
+          </h2>
+          <h2 className="inline-block hover:text-white cursor-pointer">
+            Contact
+          </h2>
         </div>
       </div>
     </div>
