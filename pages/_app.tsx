700af85380581c6dddec2bb8f175e3927ea4b73d
03kkim <theq614@gmail.com>
2023-01-12 08:54:55 -0500

    bruh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  let initial = true;
+  return (
+    <AnimatePresence mode="wait" initial={initial}>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,43 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { motion } from "framer-motion";
+import Float from "../components/Float";
+import TopbarComponent from "../components/TopbarComponent";
+import useWindowSize from "../hooks/useWindowSize";
+import { useEffect } from "react";
 
 export default function Home() {
+  const size = useWindowSize();
+  let size2 = `absolute right-[${size.width - 900}px] top-[200px]`;
+
+  useEffect(() => {
+    size2 = `absolute right-[${size.width - 900}px] top-[200px]`;
+  }, [size]);
+
+  console.log(size);
+
   return (
-    <div className="font-open-sans font-light bg-gradient-to-b from-background to-white h-screen">
-      <div className="flow-root">
-        <div className="p-10 float-left">
-          <h1 className="text-6xl">Kyu Hong Kim</h1>
-        </div>
-        <div className="p-10 space-x-10 text-4xl float-right">
-          <h2 className="inline-block hover:text-white cursor-pointer">
-            About
-          </h2>
-          <h2 className="inline-block hover:text-white cursor-pointer">Dev</h2>
-          <h2 className="inline-block hover:text-white cursor-pointer">
-            Music
-          </h2>
-          <h2 className="inline-block hover:text-white cursor-pointer">
-            Contact
-          </h2>
-        </div>
+    <div className="relative font-open-sans font-light bg-gradient-to-b from-background to-white h-screen">
+      <div className="absolute m-auto left-0 right-0 flex justify-center items-center h-full">
+        <h1 className="text-6xl">Kyu Hong Kim</h1>
+      </div>
+      <div className="absolute left-[-600px] top-[200px]">
+        <Float offset={0}>
+          <h2 className="hover:text-white cursor-pointer text-4xl">About</h2>
+        </Float>
+      </div>
+      <div className={size2}>
+        <Float offset={1}>
+          <h2 className="hover:text-white cursor-pointer text-4xl">Dev</h2>
+        </Float>
+      </div>
+      <div className="absolute left-[700px] top-[200px]">
+        <Float offset={2}>
+          <h2 className="hover:text-white cursor-pointer text-4xl">Music</h2>
+        </Float>
+      </div>
+      <div className="absolute left-[600px] top-[200px]">
+        <Float offset={3}>
+          <h2 className="hover:text-white cursor-pointer text-4xl">Contact</h2>
+        </Float>
       </div>
     </div>
   );
