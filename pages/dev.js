8d13a2a62286a885e8feba29c74a1d863fc5500c
03kkim <theq614@gmail.com>
2023-01-19 16:43:32 -0600

    add spotify embeds to music page

--- pages/dev.js
+++ pages/dev.js
@@ -14,77 +14,79 @@ export default function Dev() {
   return (
     <div className="bg">
       <Header />
-      <div className="text-[40px] text-center ">Coding Projects</div>
-      <div className="py-10 text-[32px]">Works in Progress</div>
-      <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
-        <Image
-          className="drop-shadow-lg"
-          src={encapsulateThumb}
-          width={565}
-          alt="encapsulate thumbnail"
-          style={{ border: "2px solid #999999", borderRadius: "2px" }}
-        />
-        <Image
-          className="drop-shadow-lg"
-          src={kyusiteThumb}
-          width={565}
-          alt="kyusite thumbnail"
-          style={{ border: "2px solid #999999", borderRadius: "2px" }}
-        />
-      </div>
-      <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
-        <div className="text-[24px] w-[565px]">
-          <b>03kkim/spotify-capsule</b> - A Spotify time capsule generator I'm
-          making with Micah Casey-Fusco. Spotify actually released something
-          similar (albeit not as cool 😎) after we started on the project!
-          <br />
-          <i className="text-[18px]">
-            NextJS, NextAuth, MaterialUI, Spotify API
-          </i>
+      <main className="main">
+        <div className="text-[40px] text-center ">Coding Projects</div>
+        <div className="py-10 text-[32px]">Works in Progress</div>
+        <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
+          <Image
+            className="drop-shadow-lg"
+            src={encapsulateThumb}
+            width={565}
+            alt="encapsulate thumbnail"
+            style={{ border: "2px solid #999999", borderRadius: "2px" }}
+          />
+          <Image
+            className="drop-shadow-lg"
+            src={kyusiteThumb}
+            width={565}
+            alt="kyusite thumbnail"
+            style={{ border: "2px solid #999999", borderRadius: "2px" }}
+          />
         </div>
-        <div className="text-[24px] w-[565px]">
-          <b>03kkim/kyusite</b> - This website. I made it a goal to use Figma
-          for mockups, and it definitely helped make it look neater than my
-          usual apps.
-          <br />
-          <i className="text-[18px]">NextJS, TailwindCSS, Framer Motion</i>
+        <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
+          <div className="text-[24px] w-[565px]">
+            <b>03kkim/spotify-capsule</b> - A Spotify time capsule generator I'm
+            making with Micah Casey-Fusco. Spotify actually released something
+            similar (albeit not as cool 😎) after we started on the project!
+            <br />
+            <i className="text-[18px]">
+              NextJS, NextAuth, MaterialUI, Spotify API
+            </i>
+          </div>
+          <div className="text-[24px] w-[565px]">
+            <b>03kkim/kyusite</b> - This website. I made it a goal to use Figma
+            for mockups, and it definitely helped make it look neater than my
+            usual apps.
+            <br />
+            <i className="text-[18px]">NextJS, TailwindCSS, Framer Motion</i>
+          </div>
         </div>
-      </div>
-      <div className="py-10 text-[32px]">2022</div>
-      <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
-        <Image
-          className="drop-shadow-lg"
-          src={polyrthmThumb}
-          width={565}
-          alt="polyrthm thumbnail"
-          style={{ border: "2px solid #999999", borderRadius: "2px" }}
-        />
-        <Image
-          className="drop-shadow-lg"
-          src={rustseagullThumb}
-          width={565}
-          // height={353}
-          alt="rustseagull thumbnail"
-          style={{ border: "2px solid #999999", borderRadius: "2px" }}
-        />
-      </div>
-      <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
-        <div className="text-[24px] w-[565px]">
-          <b>03kkim/polyrthm</b> - A polyrhythm generator. Polyrhythms are two
-          or more rhythms being played at the same time with different
-          frequencies. While they sound cool, they're notoriously difficult to
-          play, so I made this to help myself (and others) practice! <br />
-          <i className="text-[18px]">ReactJS, BulmaCSS, Tone.js</i>
+        <div className="py-10 text-[32px]">2022</div>
+        <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
+          <Image
+            className="drop-shadow-lg"
+            src={polyrthmThumb}
+            width={565}
+            alt="polyrthm thumbnail"
+            style={{ border: "2px solid #999999", borderRadius: "2px" }}
+          />
+          <Image
+            className="drop-shadow-lg"
+            src={rustseagullThumb}
+            width={565}
+            // height={353}
+            alt="rustseagull thumbnail"
+            style={{ border: "2px solid #999999", borderRadius: "2px" }}
+          />
         </div>
-        <div className="text-[24px] w-[565px]">
-          <b>03kkim/rustseagull </b> - A Conway’s Game of Life implementation in
-          Rust, which also generates sounds based on the board state! A
-          collaboration project with Lily Zhang and Abena Laast.
-          <br />
-          <i className="text-[18px]">Winit, Pixels, Rodio </i>
+        <div className="grid grid-rows-1 grid-cols-2 justify-items-center">
+          <div className="text-[24px] w-[565px]">
+            <b>03kkim/polyrthm</b> - A polyrhythm generator. Polyrhythms are two
+            or more rhythms being played at the same time with different
+            frequencies. While they sound cool, they're notoriously difficult to
+            play, so I made this to help myself (and others) practice! <br />
+            <i className="text-[18px]">ReactJS, BulmaCSS, Tone.js</i>
+          </div>
+          <div className="text-[24px] w-[565px]">
+            <b>03kkim/rustseagull </b> - A Conway’s Game of Life implementation
+            in Rust, which also generates sounds based on the board state! A
+            collaboration project with Lily Zhang and Abena Laast.
+            <br />
+            <i className="text-[18px]">Winit, Pixels, Rodio </i>
+          </div>
         </div>
-      </div>
-      <div className="py-10 text-[32px]">2021</div>
+        <div className="py-10 text-[32px]">2021</div>
+      </main>
     </div>
   );
 }
--- pages/music.js
+++ pages/music.js
@@ -3,7 +3,53 @@ export default function Music() {
   return (
     <div className="bg">
       <Header />
-      Music Page
+      <main className="main">
+        <div className="text-[40px] text-center ">Music Projects</div>
+        <div className="text-[32px] text-center">
+          I dedicate a lot of my free time to making music, both live and
+          produced!
+        </div>
+        <div className="pt-10 text-[32px]">HIDEOUT</div>
+        <div className="pl-10 pb-10 text-[24px]">
+          HIDEOUT is a student music group at UIUC focused on creating hip hop
+          music and pushing a positive narrative about hip hop culture. As a
+          vice president, I help to plan the direction of our weekly
+          get-togethers, social events 24-hour lock-ins, album releases, and
+          performances! I also produce instrumentals for our songs and provide
+          piano recordings for other producers. Check out our projects:
+          <div className="grid grid-cols-2 place-items-center pt-5">
+            <iframe
+              className="h-[380px] w-full px-4"
+              src="https://open.spotify.com/embed/album/0IaYzjqphpIvDHuls1nrjn?utm_source=generator&theme=0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+            <iframe
+              className="h-[380px] w-full px-4"
+              src="https://open.spotify.com/embed/album/1TQjZkUy8CYTlgd5U9ZM2h?utm_source=generator&theme=0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+          </div>
+        </div>
+
+        <div className="pt-10 text-[32px]">Improvisers Exchange</div>
+        <div className="pl-10 pb-10 text-[24px]">
+          The one drawback with music production is that there’s typically
+          little interaction between the musician and audience. As part of
+          Improvisers Exchange, an ensemble centered around improvised music,
+          I’m able to keep my spirit of live music making alive!
+        </div>
+        <div className="pt-10 text-[32px]">@kvu.sic (Instagram)</div>
+        <div className="pl-10 pb-10 text-[24px]">
+          What started as an outlet to show my friends my random piano ideas and
+          improvs ended up turning into a fulfilling way for me to start
+          releasing more serious projects and recordings. Here’s the work I’m
+          most proud of:
+        </div>
+      </main>
     </div>
   );
 }
