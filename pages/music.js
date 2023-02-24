f71ae14d9ecf38f26ac4c48705b75d8c098a2dcf
Kyu Hong Kim <03kkim@gmail.com>
2023-02-24 06:43:47 -0600

    Fix errors

--- pages/music.js
+++ pages/music.js
@@ -17,23 +17,23 @@ export default function Music() {
         <div className="pt-10 text-[32px]">HIDEOUT</div>
         <div className="pl-10 pb-10 text-[24px]">
           HIDEOUT is a student music group at UIUC focused on creating hip hop
-          music and pushing a positive narrative about hip hop culture. As a
-          vice president, I help to plan the direction of our weekly
-          get-togethers, social events 24-hour lock-ins, album releases, and
-          performances! I also produce instrumentals for our songs and provide
-          piano recordings for other producers. Check out our projects:
+          music and pushing a positive narrative about hip hop culture. As the
+          secretary, I organize our weekly admin meetings and help plan social
+          events, 24-hour lock-ins, album releases, and performances! I also
+          produce instrumentals for our songs and provide piano tracks for other
+          producers. Check out our projects:
           <div className="grid grid-cols-2 place-items-center pt-5">
             <iframe
               className="h-[380px] w-full px-4"
               src="https://open.spotify.com/embed/album/0IaYzjqphpIvDHuls1nrjn?utm_source=generator&theme=0"
-              allowfullscreen=""
+              allowFullScreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
             ></iframe>
             <iframe
               className="h-[380px] w-full px-4"
               src="https://open.spotify.com/embed/album/1TQjZkUy8CYTlgd5U9ZM2h?utm_source=generator&theme=0"
-              allowfullscreen=""
+              allowFullScreen=""
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
             ></iframe>
@@ -42,10 +42,10 @@ export default function Music() {
 
         <div className="pt-10 text-[32px]">Improvisers Exchange</div>
         <div className="pl-10 pb-10 text-[24px]">
-          The one drawback with music production is that there&apos;s typically
-          little interaction between the musician and audience. As part of
-          Improvisers Exchange I&apos;m able to enjoy making music together with
-          others in a live, improvised setting.
+          The one drawback with music production is the lack of live interaction
+          between the musician and audience. As part of Improvisers Exchange
+          I&apos;m able to enjoy making music together with others in a realtime
+          improvised setting.
         </div>
         <div className="pt-10 text-[32px]">@kvu.sic (Instagram)</div>
         <div className="pl-10 pb-10 text-[24px]">
