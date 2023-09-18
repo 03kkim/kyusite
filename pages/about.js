c9ea1380d187daf691c0d48862a89fde7d096251
Kyu Hong Kim <03kkim@gmail.com>
2023-09-18 18:54:41 -0500

    Edit about page

--- pages/about.js
+++ pages/about.js
@@ -5,14 +5,11 @@ function About() {
     <div className="bg">
       <Header />
       <main className="main">
-        <div className="text-[28px] sm:text-[32px] md:text-[40px] text-center">
-          About Me
-        </div>
+        <div className="text-[28px] sm:text-[32px] md:text-[40px] text-center">About Me</div>
         <div className="text-[20px] sm:text-[22px] md:text-[24px] text-center 2xl:px-44">
           <p>
-            Hi, I&apos;m Kyu. I&apos;m a web developer, music producer, and
-            student at UIUC studying CS + Music. Here are some links for
-            contact/info:
+            Hi, I&apos;m Kyu. I&apos;m a software engineer, music producer, and student at UIUC studying CS + Music.
+            Here are some links for contact/info:
           </p>
         </div>
         <br />
@@ -33,23 +30,16 @@ function About() {
           >
             LinkedIn
           </a>
-          <a
-            className="text-[24px]"
-            target="_blank"
-            href="https://github.com/03kkim"
-            rel="noreferrer"
-          >
+          <a className="text-[24px]" target="_blank" href="https://github.com/03kkim" rel="noreferrer">
             Github
           </a>
         </div>
         <div className="pt-10 text-[32px]">Computers</div>
         <p className="pl-10 pb-10 text-[24px]">
-          The first major project I ever made back in high school used JQuery
-          and PHP, but I&apos;ve since moved onto technologies like ReactJS
-          TailwindCSS, and Node.js for my personal projects instead for the
-          simplicity and accessibility they offer. In school and in work,
-          I&apos;ve also had notable experience using C++, SQL, Typescript,
-          MaterialUI, and Kotlin.
+          The first major project I ever made back in high school used JQuery and PHP, but I&apos;ve since moved onto
+          technologies like ReactJS TailwindCSS, and Node.js for my personal projects instead for the simplicity and
+          accessibility they offer. In school and in work, I&apos;ve also had notable experience using C++, SQL,
+          Typescript, MaterialUI, and Kotlin.
           {/* <br /> */}
           {/* <br />
           Fun story: I received my first laptop in 4th grade, and being a 4th
@@ -62,12 +52,10 @@ function About() {
         </p>
         <div className="text-[32px]">Music</div>
         <p className="pl-10 pb-10 text-[24px]">
-          I first started playing music at 7, with the classical piano. When I
-          was 13, I started playing jazz and started composing my own music
-          through improvisation. Entering college, I decided to start pursuing
-          electronic music production in order to be able to record and release
-          my own music. While it was difficult to start as a beginner for the
-          3rd time, it&apos;s been a rewarding experience.
+          I first started playing music at 7, with the classical piano. When I was 13, I started playing jazz and
+          started composing my own music through improvisation. Entering college, I decided to start pursuing electronic
+          music production in order to be able to record and release my own music. While it was difficult to start as a
+          beginner for the 3rd time, it&apos;s been a rewarding experience.
         </p>
         {/* Make this a blog post or smth idk */}
         {/* <div className="text-[32px]">Computers and Music</div>
